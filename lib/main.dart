23fccfd2cf709a1cb817a60c49f9a5b8f281bbb5
noman2002 <nomn2002@gmail.com>
2021-04-27 00:16:32 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:enroute_x/pages/login_page.dart';
+import 'package:enroute_x/utils/routes.dart';
+import 'package:enroute_x/widgets/themes.dart';
 import 'package:flutter/material.dart';
+
 
 void main() {
   return runApp(MyApp());
@@ -6,7 +10,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return MaterialApp(
+
+      themeMode: ThemeMode.system,
+      theme: MyThemes.lightTheme(context),
+      debugShowCheckedModeBanner: false,
+      darkTheme: MyThemes.darkTheme(context),
+      initialRoute: MyRoutes.homeRoute,
+      routes: {
+        "/": (context) => LoginPage(),
+        // MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        // MyRoutes.cartRoute: (context) => CartPage(),
+      },
       
     );
   }
