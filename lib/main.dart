e18204d20390bcfc81d56dbc3bbd041aba955ca1
noman2002 <nomn2002@gmail.com>
2021-04-28 17:10:09 +0530

    initial commits

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,19 @@ import 'package:enroute_x/pages/home_page.dart';
 import 'package:enroute_x/pages/login_page.dart';
 import 'package:enroute_x/utils/routes.dart';
 import 'package:enroute_x/widgets/themes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   return runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: AuthenticationWrapper(),
       themeMode: ThemeMode.system,
       // theme: MyThemes.lightTheme(context),
       debugShowCheckedModeBanner: false,
@@ -24,6 +26,13 @@ class MyApp extends StatelessWidget {
         MyRoutes.loginRoute: (context) => LoginPage(),
         // MyRoutes.cartRoute: (context) => CartPage(),
       },
+    );
+  }
+}
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+     
     );
   }
 }
