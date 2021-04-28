cbfcc2d04df42c604ed9a491ca53c761ef0dc018
noman2002 <nomn2002@gmail.com>
2021-04-28 17:24:37 +0530

    initial commits

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  String name = "";
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/loginimage.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome $name",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
