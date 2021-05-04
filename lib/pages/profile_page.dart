78810b311360bb23cb50bd7076df929b62b0ce8b
noman2002 <nomn2002@gmail.com>
2021-05-04 18:08:39 +0530

    initial commits

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,19 @@
+import 'package:enroute_x/widgets/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData.fallback(),
+        backgroundColor: Colors.orange,
+        title: "Profile Page".text.black.make(),
+      ),
+      drawer: Drawer(
+        child: MyDrawer(),
+      ),
+    );
+  }
+}
