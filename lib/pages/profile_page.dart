ca1c4f83729f3fbb8fd7e356228d67a8368feeac
noman2002 <nomn2002@gmail.com>
2021-05-21 00:08:42 +0530

    profile page commits

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -90,20 +90,13 @@ class _ProfilePageState extends State<ProfilePage> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "${user.email}",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+            Text(
+              "${user.email}",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
               ),
+              textAlign: TextAlign.left,
             ),
           ],
         ),
