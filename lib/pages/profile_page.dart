11fb95826681e6de7606c5b508e5afedb133e077
noman2002 <nomn2002@gmail.com>
2021-05-22 23:39:42 +0530

    profile page commits

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -147,6 +147,8 @@ class _ProfilePageState extends State<ProfilePage> {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
+
+                    
                   ),
                   SizedBox(
                     height: 5,
