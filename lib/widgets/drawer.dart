e7b8847089febed487ff20be399dedff2e5458e4
noman2002 <nomn2002@gmail.com>
2021-05-24 00:32:03 +0530

    profile page commits

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -14,6 +14,7 @@ class _MyDrawerState extends State<MyDrawer> {
   bool isloggedin = false;
   User? user;
 
+
   checkAuthentication() async {
     _auth.authStateChanges().listen((user) {
       if (user == null) {
